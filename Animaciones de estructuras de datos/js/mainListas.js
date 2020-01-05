786c21675f0ff695c9c55e31ff018aa6909a9f19
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-01-05 20:45:46 -0300

    Clases de nodos y flechas creadas
    Se crearon las clases de los nodos y las flechas, con el metodo de creacion de cada una

--- Animaciones de estructuras de datos/js/mainListas.js
+++ Animaciones de estructuras de datos/js/mainListas.js
@@ -1,4 +1,27 @@
-class Nodo{
-    constructor(){   
+class Arrow{
+    crearArrow(){
+        const $conteinerNodes = document.querySelector('#conteinerNodes');
+        $conteinerNodes.innerHTML = $conteinerNodes.innerHTML + `
+        <div id="containerArrow" class="d-inline-block position-relative">
+            <div id="arrow"></div>
+        </div>
+        `
     }
 }
+class Nodo{
+    constructor(valor){
+        this.valor = valor   
+    }
+    crearNodo(){
+        const $conteinerNodes = document.querySelector('#conteinerNodes');
+        $conteinerNodes.innerHTML = $conteinerNodes.innerHTML + `
+            <div id="nodo" class="bg-info d-inline-block position-relative">
+                <div id="conteinerNodo" class="d-inline-block">
+                    <p id="valorNodo" class="position-absolute mb-0 h3">${this.valor}</p>
+                </div>
+            </div>
+        `
+    }
+}
+
+
