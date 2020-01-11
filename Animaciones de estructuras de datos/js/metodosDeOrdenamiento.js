bb2b5b4f3cde3a49017ae415471d60c3ba12b630
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-01-11 02:35:34 -0300

    Implementaciones de clases nuevas, y mejoras en animacion

--- Animaciones de estructuras de datos/js/metodosDeOrdenamiento.js
+++ Animaciones de estructuras de datos/js/metodosDeOrdenamiento.js
@@ -17,6 +17,49 @@ function quickSort(array){
     return (new Array).concat(quickSort(arrayIzquierda),pivote,quickSort(arrayDerecha));
 }
 
+
+function hacerCosas(){
+    const $conteiners = document.querySelector('#conteiners');
+    let conteinerDos = new conteinerNodo('conteinerNodesDos').crearConteiner();
+    let nodoAnimate = new NodoAnimate('nodoPivote').crearNodoAnimate();
+
+    conteinerDos.appendChild(nodoAnimate)
+    $conteiners.appendChild(conteinerDos)
+
+    conteinerDos = new conteinerNodo('conteinerNodesDiv').crearConteiner();
+    let $divColIzquierda = (new conteinerNodo).crearColumnas();
+    let $divColDerecha = (new conteinerNodo).crearColumnas();
+
+    nodoAnimate = new NodoAnimate('nodo').crearNodoAnimate();
+    $divColIzquierda.appendChild(nodoAnimate);
+    
+
+    nodoAnimate = new NodoAnimate('nodoDerecha').crearNodoAnimate();
+    $divColDerecha.appendChild(nodoAnimate);
+
+
+    conteinerDos.classList.add('row');
+    conteinerDos.appendChild($divColIzquierda);
+    conteinerDos.appendChild($divColDerecha);
+
+    $conteiners.appendChild(conteinerDos);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function prueba(){
     let x = [3,4,5,6,0,2,7,4,8,9]
     let ui = new UI()
